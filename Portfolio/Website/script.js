4c63c722957830b79e39d645be82fbc78c0d2d1d
margaretfhs <margaretfhsullivan@gmail.com>
2025-11-10 14:11:17 -0600

    Initial Files

---
+++ Portfolio/Website/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('showBtn').addEventListener('click', function () {
+  document.getElementById('msg').textContent = 'You just uploaded a webpage — well done!';
+});
